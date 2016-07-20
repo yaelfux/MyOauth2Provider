fcd7269070b3e5f16501ebcc33b0591489600e87
yaelfux <yaelmah01@gmail.com>
2016-07-20 18:27:49 +0300

    Update DemoOauth2Application.java

--- src/main/java/yael/example/DemoOauth2Application.java
+++ src/main/java/yael/example/DemoOauth2Application.java
@@ -5,7 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class DemoOauth2Application {
+public class DemoOauth2Application extends WebSecurityConfigurerAdapter{
+
+  @RequestMapping("/user")
+  public Principal user(Principal principal) {
+    return principal;
+    
+     @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .antMatcher("/**")
+      .authorizeRequests()
+        .antMatchers("/", "/login**", "/webjars/**")
+        .permitAll()
+      .anyRequest()
+        .authenticated();
+  }
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoOauth2Application.class, args);
